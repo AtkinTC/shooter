3fcd910ab3ad23cbaa72a53764bc7851793e7d1a
AtkinTC <Atkintc@gmail.com>
2015-07-18 23:17:30 -0400

    unified entities in dictionary

--- spaceshooter.py
+++ spaceshooter.py
@@ -13,39 +13,48 @@ from shape import *
 from random import randint
 from pygame.time import Clock
 
-#test
-
-pygame.init()
 
 clock = 0
 
-size = width, height = 640, 480
+game_dimensions = width, height = 0, 0
 
-draw.init(width,height)
+entities = {}
 
 def t_add(t1,t2):
     return map(lambda a,b: a+b,t1,t2)
 
+def init():
+    global clock, game_dimensions, width, height, entities
+
+    pygame.init()
+
+    clock = 0
+
+    game_dimensions = width, height = 640, 480
+
+    ship = draw.load_image('ship.tif', True)
+    dust1 = draw.load_image('layers\dust_640x480_1.tif', True)
+    dust2 = draw.load_image('layers\dust_640x480_2.tif', True)
+    dust3 = draw.load_image('layers\dust_800x800_1.tif', True)
+    planet1 = draw.load_image('layers\planet_154x154_1.tif', True)
+    planet2 = draw.load_image('layers\planet_40x40_1.tif', True)
+    starfield = draw.load_image('layers\starfield_640x480_1.tif', True)
+
+    entities = {}
+    shape = Polygon([Pnt(0,-15),Pnt(10,15), Pnt(-10,15)], Pnt())
+    entities['player'] = entity.Player(ship, shape, 0.001, 0.3, 0.005)
+
+    entities['back1'] = background.Background_scrolling(640, 480, dust1, 0.5, 4)
+    entities['back2'] = background.Background_scrolling(640, 480, dust2, 1.0, 3)
+    entities['back3'] = background.Background_scrolling(800, 800, dust3, 2.0, 3)
+    entities['back_planet1'] = background.Background_object(300, 100, 154, 154, planet1, 4, 2)
+    entities['back_planet2'] = background.Background_object(550, -200, 40, 40, planet2, 6, 1)
+    entities['back4'] = background.Background_scrolling(640, 480, starfield, 20, 0)
+
+    draw.init(width, height)
 
 
-ship = draw.load_image('ship.tif', True)
-dust1 = draw.load_image('layers\dust_640x480_1.tif', True)
-dust2 = draw.load_image('layers\dust_640x480_2.tif', True)
-dust3 = draw.load_image('layers\dust_800x800_1.tif', True)
-planet1 = draw.load_image('layers\planet_154x154_1.tif', True)
-planet2 = draw.load_image('layers\planet_40x40_1.tif', True)
-starfield = draw.load_image('layers\starfield_640x480_1.tif', True)
-
-shape = Polygon([Pnt(0,-15),Pnt(10,15), Pnt(-10,15)], Pnt())
-
-player = entity.Player(ship, shape, 0.001, 0.3, 0.005)
-
-back1 = background.Background_scrolling(640, 480, dust1, 0.5, 4)
-back2 = background.Background_scrolling(640, 480, dust2, 1.0, 3)
-back3 = background.Background_scrolling(800, 800, dust3, 2.0, 3)
-back_planet1 = background.Background_object(300, 100, 154, 154, planet1, 4, 2)
-back_planet2 = background.Background_object(550, -200, 40, 40, planet2, 6, 1)
-back5 = background.Background_scrolling(640, 480, starfield, 20, 0)
+init()
 
 count = 0
 #main loop
@@ -88,31 +97,22 @@ while not done:
                 player_move.x += -1
 
     if player_move.mag():
-        player.input_accel(player_move)
+        entities['player'].input_accel(player_move)
 
-    player.input_aim(mpos+draw.get_camera()-Pnt(width/2,height/2))
+    entities['player'].input_aim(mpos+draw.get_camera()-Pnt(width/2,height/2))
 
     #update loop
-    player.update(delta)
-    back1.update()
-    back2.update()
-    back3.update()
-    #back4.update()
-    back5.update()
+    for e in entities:
+        e.update(delta)
 
-    draw.camera_set(player.pos-player.velocity*delta)
+    draw.camera_set(entities['player'].pos-player.velocity*delta)
     
     #draw loop
 
     draw_list = Draw_Call_List()
 
-    draw_list.append(back5.draw())
-    draw_list.append(back_planet2.draw())
-    draw_list.append(back_planet1.draw())
-    draw_list.append(back3.draw())
-    draw_list.append(back2.draw())
-    draw_list.append(back1.draw())
-    draw_list.append(player.draw())
+    for e in entities:
+        draw_list.append(e.draw())
 
     draw_list.draw()
               
