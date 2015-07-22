431d7d6489ed4aae1dc32c89c239ee0cf29b5064
AtkinTC <Atkintc@gmail.com>
2015-07-22 14:05:10 -0400

    combined draw_call and draw_call_list

--- draw_call.py
+++ draw_call.py
@@ -1,4 +1,4 @@
-﻿from draw import *
+﻿import draw
 
 class Draw_call:
     def __init__(self, type, depth):
@@ -9,13 +9,36 @@ class Draw_call:
     def set_arg(self, arg, value):
         self.args[arg] = value
 
-    def run_draw_call(self):
+    def run(self):
         type = self.type
-        if type == 'image':
-            draw_image(**self.args)
+        if type == 'texture':
+            draw.draw_texture_gl(**self.args)
+        elif type == 'image':
+            draw.draw_image(**self.args)
         elif type == 'shape':
-            draw_shape(**self.args)
+            draw.draw_shape(**self.args)
         elif type == 'rect':
-            draw_rect(**self.args)
+            draw.draw_rect(**self.args)
         elif type == 'text':
-            draw_text(**self.args)
+            draw.draw_text(**self.args)
+
+class Draw_Call_List:
+    def __init__(self):
+        self.dict = {}
+
+    def append(self, calls):
+        for call in calls:
+            if self.dict.has_key(call.depth):
+                self.dict[call.depth].append(call)
+            else:
+                self.dict[call.depth] = [call]
+
+    def draw(self):
+        for k in self.dict.keys():
+            for call in self.dict[k]:
+                call.draw()
+
+    def ordered_calls(self):
+        for k in self.dict.keys():
+            for call in self.dict[k]:
+                yield call
